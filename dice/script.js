fda1bc0a2a1240e4bff74e2168293ed94ab8f373
enayatullah azizzada <e_azizzada@yahoo.com>
2023-01-26 11:09:08 -0500

    final2

--- dice/script.js
+++ dice/script.js
@@ -4,8 +4,10 @@ const player0Element = document.querySelector(".player--0");
 const player1Element = document.querySelector(".player--1");
 const score0Element = document.querySelector("#score--0");
 const score1Element = document.querySelector("#score--1");
+
 const currentScore0Element = document.querySelector("#current--0");
 const currentScore1Element = document.querySelector("#current--1");
+
 const dice = document.querySelector(".dice");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
